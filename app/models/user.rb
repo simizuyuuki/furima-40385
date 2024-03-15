8f422f291d148b4df67e032bf8438d45cc56a2a1
simizuyuuki <riiman8713@gmail.com>
2024-03-15 17:24:05 +0900

    ユーザーモデルテスト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/}
-  validates :encrypted_password, presence: true
-  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name, presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_hurigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_hurigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_hurigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :nickname, presence: true
   validates :date_of_birth, presence: true
 
-  has_many :items
+  #has_many :items
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,6 +99,20 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)") 
       end
 
+      it 'passwordは、数字のみでは登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
+      it 'passwordは、全角だと登録できない' do
+        @user.password = '12345あ'
+        @user.password_confirmation = '12345あ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+
       it 'passwordは、半角英数字混合でなければ登録できない' do
         @user.password = 'password'
         @user.password_confirmation = 'password'
