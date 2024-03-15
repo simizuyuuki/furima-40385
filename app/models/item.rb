425322fa38d0ca76bbcd25774e631f9b6c480dd0
simizuyuuki <riiman8713@gmail.com>
2024-03-15 21:07:10 +0900

    itemモデルのバリデーション設定（正規表現なし）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :situation, presence: true
+  validates :charge, presence: true
+  validates :prefectur, presence: true
+  validates :number, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
 
 end
