584d2c493f94ca699984b9a1928d580e0aabbdd3
simizuyuuki <riiman8713@gmail.com>
2024-03-17 17:26:56 +0900

    itemモデルバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
-
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :prefectur
+  belongs_to :number
+  belongs_to :change
+  
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id,  numericality: { other_than: 1 , message: "can't be blank"}
-  validates :situation_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefectur_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :number_id,  numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
-
+  validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :situation_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefectur_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :number_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates_format_of :price, with: /\A[0-9]+\z/, message: "は半角数字のみです"
+  
   belongs_to :user
-
 end
