633169fd19fd9c5aa4d087cf48da9bf090a23e6b
simizuyuuki <riiman8713@gmail.com>
2024-03-18 14:56:51 +0900

    販売手数料の記述

---
+++ app/javascript/commission.js
@@ -0,0 +1,20 @@
+function commission() {
+  const priceInput = document.getElementById("item-price");
+  const addTaxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  priceInput.addEventListener('input', function() {
+    let price = parseInt(priceInput.value, 10);
+    if (isNaN(price)) {
+      price = 0;
+    }
+    
+    const tax = Math.floor(price * 0.1);
+    const profitAmount = price - tax;
+
+    addTaxPrice.textContent = `${tax.toLocaleString()}`;
+    profit.textContent = `${profitAmount.toLocaleString()}`;
+  });
+};
+
+window.addEventListener('turbo:load', commission)
